583cf88de4770ab2fcd71f952979c169c520cd34
Valentino419 <andradevalentino419@gmail.com>
2024-04-04 21:24:18 -0300

    nuevos botones

--- Js/funciones.js
+++ Js/funciones.js
@@ -1,3 +1,5 @@
 // JavaScript source code
 //alert("hola mundo")
-function Saludar(nombre){alert("hola"+nombre)}
+function Saludar(nombre){alert("hola "+nombre)}
+const Aux= 72;
+function Comparar(numero){ if(numero<Aux){alert("numero es menor")} else {alert("es mayor")}}
