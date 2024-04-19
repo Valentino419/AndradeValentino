a1a47f5f8dd7e759184b1ef908ba76a255f4ff73
valentino419 <andradevalentino419@gmail.com>
2024-04-19 20:54:12 -0300

    implementacion del mapa y funcion de busqueda

--- TP3/fn.js
+++ TP3/fn.js
@@ -1,6 +1,25 @@
+var map = L.map('map').setView([-34.61315,-58.38], 13);
 
-const myObj = JSON.parse(fetch('https://randomuser.me/api/')); 
-document.getElementById("demo").innerHTML = myObj.name
-    
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
- 
+
+
+
+
+
+function BuscarUsuario() {
+  fetch('https://randomuser.me/api/').then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+   var marker = L.marker([data.results[0].location.coordinates.latitude,data.results[0].location.coordinates.longitude]).addTo(map)
+    .bindPopup(data.results[0].name.title+" "+data.results[0].name.first+" "+data.results[0].name.last)
+    .openPopup();
+
+
+
+    console.log(data.results[0].location.coordinates)
+  })
+}
